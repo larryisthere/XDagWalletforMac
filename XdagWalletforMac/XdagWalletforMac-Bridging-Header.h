9e7cdc095a58a03d5bffa6f1293b0822a414a3fe
Larry Zhang <larryisthere@gmail.com>
2019-04-09 08:51:30 +0800

    Submoduled xDagWallet

---
+++ XdagWalletforMac/XdagWalletforMac-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Xdag.h
+//  XdagWallet
+//
+//  Created by 张晓亮 on 2019/4/8.
+//  Copyright © 2019 张晓亮. All rights reserved.
+//
+
+#ifndef XdagWalletforMac_Bridging_Header_h
+#define XdagWalletforMac_Bridging_Header_h
+
+#include "xDagWallet/src/client/xdag_wrapper.h"
+#include "xDagWallet/src/client/cli.h"
+#include "xDagWallet/src/client/utils/utils.h"
+#include <ifaddrs.h>
+
+#endif /* XdagWalletforMac_Bridging_Header_h */
